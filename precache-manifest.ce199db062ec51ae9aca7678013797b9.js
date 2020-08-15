221a32a12ce3c47c696076f620fcc3fbf01f377a
leeseokwoon <leeseokwoon@naver.com>
2020-08-15 18:54:02 +0900

    Updates

---
+++ precache-manifest.ce199db062ec51ae9aca7678013797b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1279b9758408603757d4003d3be1010",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "0b226fa8acd2b303d8c4",
+    "url": "/myportfolio/static/css/main.5c404e7e.chunk.css"
+  },
+  {
+    "revision": "10ea567deb6179864864",
+    "url": "/myportfolio/static/js/2.5422f886.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myportfolio/static/js/2.5422f886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b226fa8acd2b303d8c4",
+    "url": "/myportfolio/static/js/main.88a6ca55.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  },
+  {
+    "revision": "a52a00cf3f7dcd029e1537aac11b8f32",
+    "url": "/myportfolio/static/media/Asem.a52a00cf.woff"
+  },
+  {
+    "revision": "82f155a13554215764725f704141fbf8",
+    "url": "/myportfolio/static/media/android.82f155a1.gif"
+  },
+  {
+    "revision": "8d1e6a5bf7c52da5ad448c8ee353016f",
+    "url": "/myportfolio/static/media/hobby.8d1e6a5b.gif"
+  },
+  {
+    "revision": "cb3f77b32a2d611ab65a957c0ccdcf57",
+    "url": "/myportfolio/static/media/react_moive.cb3f77b3.PNG"
+  }
+]);
